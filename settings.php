8ae30567df199f60970067b585182e512456fc5a
avi.megladon <avi.megladon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-02 16:37:43 +0000

    update..
    git-svn-id: https://plugins.svn.wordpress.org/login-sidebar-widget/tags/2.2.3@942289 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- settings.php
+++ settings.php
@@ -234,7 +234,7 @@ class login_settings {
 	function fb_login_pro_add(){ ?>
 	<table width="98%" border="0" style="background-color:#FFFFD2; border:1px solid #E6DB55; padding:0px 0px 0px 10px; margin:2px;">
   <tr>
-    <td><p>There is a PRO version of this plugin that supports login with <strong>Facebook</strong>, <strong>Google</strong> And <strong>Twitter</strong>. You can get it <a href="http://aviplugins.com/fb-login-widget-pro/" target="_blank">here</a> in <strong>USD 1.00</strong> </p></td>
+    <td><p>There is a PRO version of this plugin that supports login with <strong>Facebook</strong>, <strong>Google</strong>,  <strong>Twitter</strong> and <strong>LinkedIn</strong>. You can get it <a href="http://aviplugins.com/fb-login-widget-pro/" target="_blank">here</a> in <strong>USD 1.50</strong> </p></td>
   </tr>
 </table>
 	<?php }
